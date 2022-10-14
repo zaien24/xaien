4495a614e857d8e4894f6424a035e73c6ae261ff
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-15 08:41:31 +0900

    p479 시큐리티 설정 클래스 작성

---
+++ src/main/java/com/zaien/xaien/cconfig/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.zaien.xaien.cconfig;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
