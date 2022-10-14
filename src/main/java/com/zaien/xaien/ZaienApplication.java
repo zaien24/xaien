3ed9e565c5e5b649d095fb282e11eeb92019260e
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-15 07:52:46 +0900

    Initial commit

---
+++ src/main/java/com/zaien/xaien/ZaienApplication.java
@@ -0,0 +1,13 @@
+package com.zaien.xaien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaienApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaienApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zaien/xaien/ZaienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaien.xaien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaienApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
