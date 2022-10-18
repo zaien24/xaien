c8927c5c9ba3c08ff1985400668ac443aa9ce9b1
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-19 01:41:27 +0900

    p506 Reposity와 더미데이터 추가하기

---
+++ src/main/java/com/zaien/xaien/repository/ClubMemberRepository.java
@@ -0,0 +1,13 @@
+package com.zaien.xaien.repository;
+
+
+import com.zaien.xaien.entity.ClubMember;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface ClubMemberRepository extends JpaRepository<ClubMember, String> {
+
+}
---
+++ src/test/java/com/zaien/xaien/security/ClubMemberTests.java
@@ -0,0 +1,53 @@
+package com.zaien.xaien.security;
+
+import com.zaien.xaien.entity.ClubMember;
+import com.zaien.xaien.entity.ClubMemberRole;
+import com.zaien.xaien.repository.ClubMemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class ClubMemberTests {
+
+    @Autowired
+    private ClubMemberRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void insertDummies() {
+        //1 - 80까지는 USER만 지정
+        //81- 90까지는 USER,MANAGER
+        //91- 100까지는 USER,MANAGER,ADMIN
+
+        IntStream.rangeClosed(1,100).forEach(i -> {
+            ClubMember clubMember = ClubMember.builder()
+                    .email("user"+i+"@zerock.org")
+                    .name("사용자"+i)
+                    .fromSocial(false)
+                    .roleSet(new HashSet<ClubMemberRole>())
+                    .password(  passwordEncoder.encode("1111") )
+                    .build();
+
+            //default role
+            clubMember.addMemberRole(ClubMemberRole.USER);
+
+            if(i > 80){
+                clubMember.addMemberRole(ClubMemberRole.MANAGER);
+            }
+
+            if(i > 90){
+                clubMember.addMemberRole(ClubMemberRole.ADMIN);
+            }
+
+            repository.save(clubMember);
+
+        });
+    }
+}
