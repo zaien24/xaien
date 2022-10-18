d65e43f94ad38f1a0879b8e1484d77e9662bfac4
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-19 01:13:34 +0900

    p496

--- src/main/java/com/zaien/xaien/cconfig/SecurityConfig.java
+++ src/main/java/com/zaien/xaien/cconfig/SecurityConfig.java
@@ -22,7 +22,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/sample/all").permitAll();
+                .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
+
+        http.formLogin(); //인가/인증에 문제시 로그인 화면
     }
 
     @Override
