4b8f205b207b72186dfcaeee08c0272a12176225
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-19 01:11:53 +0900

    p494 인가가 필요한 리소스 설정

--- src/main/java/com/zaien/xaien/cconfig/SecurityConfig.java
+++ src/main/java/com/zaien/xaien/cconfig/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +16,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/sample/all").permitAll();
     }
 
     @Override
