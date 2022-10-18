16dfb0761b0ee9907616ff9515e115b45174da99
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-19 01:10:33 +0900

    p493

--- src/main/java/com/zaien/xaien/cconfig/SecurityConfig.java
+++ src/main/java/com/zaien/xaien/cconfig/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.zaien.xaien.cconfig;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,5 +15,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication().withUser("user1") //사용자 계정은 user1
+                .password("$2a$10$qbTVRGiC8RePIsMz4z/QP.LjBmLOMGXBCkmW2comzfNaoeidd5/aa") //1111 패스워드 인코딩
+                .roles("USER");
+
     }
 }
