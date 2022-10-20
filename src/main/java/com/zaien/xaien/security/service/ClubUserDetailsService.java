ff0a83b8e4a368a578407f47cde54c12d2538e62
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-21 02:04:49 +0900

    p515 UserDetialsSerivce 구현

---
+++ src/main/java/com/zaien/xaien/security/service/ClubUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.zaien.xaien.security.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+public class ClubUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        log.info("ClubUserDetailsService loadUserByUsername " + username);
+
+        return null;
+    }
+
+}
