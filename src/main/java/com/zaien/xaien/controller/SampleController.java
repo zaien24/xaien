d60c0a3a76093afc53a943a45939df918c99d0de
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-21 02:14:43 +0900

    p522 컨트롤러에서의 출력

--- src/main/java/com/zaien/xaien/controller/SampleController.java
+++ src/main/java/com/zaien/xaien/controller/SampleController.java
@@ -1,6 +1,8 @@
 package com.zaien.xaien.controller;
 
+import com.zaien.xaien.security.dao.ClubAuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,13 @@ public class SampleController {
     }
 
     @GetMapping("/member")
-    public void exMember(){
+    public void exMember(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember){
+
         log.info("exMember..........");
+
+        log.info("-------------------------------");
+        log.info(clubAuthMember);
+
     }
 
     @GetMapping("/admin")
