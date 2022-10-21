b6062de22240e8587eb22ee7c4fe6997570c1fac
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-21 15:04:57 +0900

    p554 Remember me 설정

--- src/main/java/com/zaien/xaien/config/SecurityConfig.java
+++ src/main/java/com/zaien/xaien/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.zaien.xaien.config;
 
 import com.zaien.xaien.security.handler.ClubLoginSuccessHandler;
+import com.zaien.xaien.security.service.ClubUserDetailsService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +14,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @Log4j2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ClubUserDetailsService userDetailsService;
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -27,9 +32,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.formLogin(); //인가/인증에 문제시 로그인 화면
         http.csrf().disable();
-
+        http.logout();
         http.oauth2Login().successHandler(successHandler());
-//        http.logout();
+        http.rememberMe().tokenValiditySeconds(60*60*7).userDetailsService(userDetailsService);  //7days
+
     }
 
     @Bean
