a52de9c36206bf4c10b8b42bbf85876d6d5cd993
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-21 14:39:09 +0900

    p551 RedirectStrategy를 이용한 리다렉트

--- src/main/java/com/zaien/xaien/config/SecurityConfig.java
+++ src/main/java/com/zaien/xaien/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.zaien.xaien.security.handler.ClubLoginSuccessHandler;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -35,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public ClubLoginSuccessHandler successHandler() {
-        return new ClubLoginSuccessHandler();
+        return new ClubLoginSuccessHandler(passwordEncoder());
     }
 
 //    @Override
