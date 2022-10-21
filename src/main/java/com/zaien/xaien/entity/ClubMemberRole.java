f6deddada3081b3979327cd7bc19ff9d14eb1f0e
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-21 13:38:53 +0900

    p540 이메일을 이용한 회원 가입 처리

--- src/main/java/com/zaien/xaien/entity/ClubMemberRole.java
+++ src/main/java/com/zaien/xaien/entity/ClubMemberRole.java
@@ -3,4 +3,5 @@ package com.zaien.xaien.entity;
 public enum ClubMemberRole {
 
     USER,MANAGER,ADMIN
+
 }
--- src/main/java/com/zaien/xaien/security/service/ClubAuth2UserDetailsService.java
+++ src/main/java/com/zaien/xaien/security/service/ClubAuth2UserDetailsService.java
@@ -1,18 +1,35 @@
 package com.zaien.xaien.security.service;
 
+import com.zaien.xaien.entity.ClubMember;
+import com.zaien.xaien.entity.ClubMemberRole;
+import com.zaien.xaien.repository.ClubMemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class ClubAuth2UserDetailsService extends DefaultOAuth2UserService {
+
+    private final ClubMemberRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
+
+
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        log.info("--------------------------------------");
+        log.info("userRequest:" + userRequest);
 
         String clientName = userRequest.getClientRegistration().getClientName();
 
@@ -26,7 +43,41 @@ public class ClubAuth2UserDetailsService extends DefaultOAuth2UserService {
             log.info(k +":" + v);
         });
 
+        String email = null;
+
+        if(clientName.equals("Google")){
+            email = oAuth2User.getAttribute("email");
+        }
+
+        log.info("EMAIL: " + email);
+
+        ClubMember member = saveSocialMember(email);
+
         return oAuth2User;
+    }
+
+    private ClubMember saveSocialMember(String email){
+        //기존에 동일한 이메일로 가입한 회원이 있는 경우에는 그대로 조회만
+        Optional<ClubMember> result = repository.findByEmail(email, true);
+
+        if(result.isPresent()){
+            return result.get();
+        }
+
+        //없다면 회원 추가 패스워드는 1111 이름은 그냥 이메일 주소로
+        ClubMember clubMember = ClubMember.builder().email(email)
+                .name(email)
+                .password( passwordEncoder.encode("1111") )
+                .fromSocial(true)
+                .build();
+
+        clubMember.addMemberRole(ClubMemberRole.USER);
+
+
+        repository.save(clubMember);
+
+        return clubMember;
+
     }
 
 }
