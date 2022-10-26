0a7ce15ef6a01de09d50e07fcfb871d1253c2359
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-27 00:41:52 +0900

    p594 JWT 토큰 생성 / 검증

---
+++ src/test/java/com/zaien/xaien/security/JWTTests.java
@@ -0,0 +1,41 @@
+package com.zaien.xaien.security;
+
+import com.zaien.xaien.security.util.JWTUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class JWTTests {
+
+    private JWTUtil jwtUtil;
+
+    @BeforeEach
+    public void testBefore() {
+
+        System.out.println("testBefore.............");
+        jwtUtil = new JWTUtil();
+    }
+
+    @Test
+    public void testEncode() throws Exception {
+        String email = "user95@zerock.org";
+
+        String str = jwtUtil.generateToken(email);
+
+        System.out.println(str);
+    }
+
+    @Test
+    public void testValidate() throws Exception {
+
+        String email = "user95@zerock.org";
+
+        String str = jwtUtil.generateToken(email);
+
+        Thread.sleep(5000);
+
+        String resultEmail = jwtUtil.validateAndExtract(str);
+
+        System.out.println(resultEmail);
+
+    }
+}
