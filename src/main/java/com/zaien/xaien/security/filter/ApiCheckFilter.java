40a0bb2902bad551da358ad248e4b1e944245209
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-27 00:02:45 +0900

    p587 헤더 검증 실패 처리

--- src/main/java/com/zaien/xaien/security/filter/ApiCheckFilter.java
+++ src/main/java/com/zaien/xaien/security/filter/ApiCheckFilter.java
@@ -1,6 +1,7 @@
 package com.zaien.xaien.security.filter;
 
 import lombok.extern.log4j.Log4j2;
+import net.minidev.json.JSONObject;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -10,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Log4j2
 public class ApiCheckFilter extends OncePerRequestFilter {
@@ -41,8 +43,19 @@ public class ApiCheckFilter extends OncePerRequestFilter {
             if(checkHeader){
                 filterChain.doFilter(request, response);
                 return;
+            } else {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                // json 리턴
+                response.setContentType("application/json;charset=utf-8");
+                JSONObject json = new JSONObject();
+                String message = "FAIL CHECK API TOKEN";
+                json.put("code", "403");
+                json.put("message", message);
+
+                PrintWriter out = response.getWriter();
+                out.print(json);
+                return;
             }
-            return;
         }
 
         filterChain.doFilter(request, response);
